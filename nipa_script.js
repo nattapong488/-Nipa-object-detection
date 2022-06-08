90129aaddfecfba3a1e441cf6260bc230ea938b5
james <nattapong_soponwattana@hotmail.com>
2022-06-08 18:01:11 +0700

    delete key

--- nipa_script.js
+++ nipa_script.js
@@ -90,7 +90,7 @@ click_button.addEventListener('click', function () {
 // import nvision from "./@nipacloud/nvision"
 
 // const objectDetectionService = nvision.objectDetection({
-//     apiKey: "cdb29f355cb4059995e05420dc8d963f657898bf3a5f2f5e7a88c58279f5e4a0a1c4c4cf874594b42e413fc45c425425ac"
+//     apiKey: ""
 // });
 
 // objectDetectionService.predict({
